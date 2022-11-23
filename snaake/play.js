dcb5ca769c9cd08034c244bd006c98a987ba7f27
Dango3010 <thuyoanh199930@gmail.com>
2022-11-23 17:51:34 +0000

    changes

---
+++ snaake/play.js
@@ -0,0 +1,9 @@
+//client.js
+const net = require("net");
+
+const connect = require('./client')
+
+console.log("Connecting ...");
+connect();
+//The game server automatically places your snake on the board as soon as you establish the connection. you should now see your snake appear briefly on the server's game screen.
+//
