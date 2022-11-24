51479e4efce3d6efb42a900687507ea1931ad909
Dango3010 <thuyoanh199930@gmail.com>
2022-11-24 03:04:18 +0000

    create a connection object and binding keys

--- input.js
+++ input.js
@@ -1,17 +1,27 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 //the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function (key) {
-    if (key === '\u0003') { // \u0003 maps to ctrl+c input
-      process.exit();
-    }
-  };
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+const movement = {w: 'up', a: 'left', s: 'down', d: 'right'};
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+    process.exit();
+  }
+  if (movement[key]) {
+    console.log(`going ${movement[key]}`);
+    connection.write(`Move: ${movement[key]}`);
+  }
 };
 
 module.exports = setupInput;
--- play.js
+++ play.js
@@ -5,7 +5,7 @@ const connect = require('./client');
 const setupInput = require('./input');
 
 console.log("Connecting ...");
-connect();
 //The game server automatically places your snake on the board as soon as you establish the connection. you should now see your snake appear briefly on the server's game screen.
 
-setupInput();
+setupInput(connect());
+
